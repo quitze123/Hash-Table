aa08f9a948c0fc6444168edc5340eb6efa1be218
Jose Flores-Martinez <git@github.com:quitze123/lab_2_CSC349.git>
2018-09-06 16:00:58 -0700

    Added tests functions.

--- src/main.c
+++ src/main.c
@@ -7,14 +7,28 @@
 #include "ui.h"
 #include "test_functions.h"
 
+#define TEST 1
+#define RUN_TEST 0
+
+enum Tests
+{
+   INSERT, LOOKUP, REMOVE
+};
 
 int main(void)
 {
    void * table_info = run();
-   test_insert(&table_info);
-   /*
-   interface(&table_info);
-   */
+
+   if(TEST)
+   {
+      if(RUN_TEST == INSERT)
+         test_insert(&table_info);
+   }
+   else
+   {
+      interface(&table_info);
+   }
+
    free_table(table_info);
    return 0;
 }
